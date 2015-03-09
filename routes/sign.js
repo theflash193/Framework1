5b92f7c0f421d6f5aafa58fc7c5b55657cada64c
Gordon RASS-KWASI <grass-kw@e1r2p18.42.fr>
2015-03-09 16:23:39 +0100

    ajout d'une page de creation de compte pour l'utilisateur

--- routes/sign.js
+++ routes/sign.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var	passport = require('passport');
+var form = require("../form/user");
 
 /* GET login page. */
 router.get('', function(req, res, next) {
   res.render('sign');
 });
 
-router.post('sign',
+router.get('/new', function(req, res) {
+	res.render('sign/new', {form: form.UserForms.toHTML()});
+})
+
+router.post('/new',
 	passport.authenticate('local', {successRedirect: '/',
 	failureRedirect: '/login',
 	failureFlash: true}
