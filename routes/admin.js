5ece5ef013fa31e0a2d917ffc9ac15da949c0562
Gordon RASS-KWASI <grass-kw@e1r2p18.42.fr>
2015-03-09 12:33:31 +0100

    suppresion de tous les fichier

--- routes/admin.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET admin page */
-router.get('/admin', function(req, res) {
-	res.render('admin/admin');
-});
-
-module.exports = router;
