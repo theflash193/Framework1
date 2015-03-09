b739e990c6f46f9fa80eb6c0c7498b8639b4e6ed
Gordon RASS-KWASI <grass-kw@e1r2p18.42.fr>
2015-03-09 16:07:37 +0100

    creation de page CRUD, avec le formulaire utilisateur

---
+++ form/user.js
@@ -0,0 +1,26 @@
+var forms = require('forms');
+var fields = forms.fields;
+var validators = forms.validators;
+var widget = forms.widgets;
+
+var userForms = forms.create({
+	username: fields.string({required: true, label: 'Username'}),
+	password: fields.password({required: true}),
+	confirm: fields.password({
+		required: true,
+		validators: [validators.matchField('password')]
+	}),
+	personal: {
+		name: fields.string({required: true, label: 'Name'}),
+		email: fields.email({required: true, label: 'Email'}),
+		address: {
+			address1: fields.string({required: true, label: 'address1'}),
+			address2: fields.string({label: 'address2'}),
+			city: fields.string({required: true, label: 'City'}),
+			state: fields.string({required: true, label: 'State'}),
+			zip: fields.string({required: true, label: 'Zip'})
+		}
+	}
+});
+
+UserForms = exports.UserForms = userForms;
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+var form = require("../form/user");
+
+/* function define */
+function create(req, res)
+{
+	console.log(UserForms);
+	res.render('admin/create', {
+		form : form.UserForms.toHTML()}
+		);
+}
+
+function read(req, res)
+{
+	res.render('admin/read');
+}
+
+function update(req, res)
+{
+	res.render('admin/update');
+}
+
+function deletead(req, res)
+{
+	res.render('admin/delete');
+}
 
 /* GET admin page */
-router.get('/admin', function(req, res) {
-	res.render('admin');
+router.get('/', function(req, res) {
+	res.render('admin/admin');
 });
 
+router.get('/create', create);
+router.get('/read', read);
+router.get('/update', update);
+router.get('/delete', deletead);
 module.exports = router;
